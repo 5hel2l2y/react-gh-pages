b0646e1a32446100e226383ba7891e81a3511d2c
5hel2l2y <sherry@junocollege.com>
2019-11-28 10:54:20 -0500

    Updates

---
+++ precache-manifest.c1a1ceb90fcb3483a4fa46fdc9dd0e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1156f24de068555273ec06db381d621c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c70be675af4d5581c7c9",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aade0aaec43e82c673d9",
+    "url": "/react-gh-pages/static/js/2.0c792337.chunk.js"
+  },
+  {
+    "revision": "c70be675af4d5581c7c9",
+    "url": "/react-gh-pages/static/js/main.1b69b916.chunk.js"
+  },
+  {
+    "revision": "3703c47d598af9a7e5b4",
+    "url": "/react-gh-pages/static/js/runtime-main.ead2228f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
